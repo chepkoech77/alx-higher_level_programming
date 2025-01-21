bff426b418fc889e1303c55d89b07d138fae5914
chepkoech77 <gracejohnson.koech@gmail.com>
2025-01-21 22:59:24 +0300

    servers

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-:x
-# cURL get body size
-curl -sI "$1" | grep -i Content-Length | cut -d ' ' -f 2
+#!/bin/bash
+# This script takes a URL, sends request and displays
+curl -sI "$1" | grep -i Content-Length | cut -d " " -f 2 
